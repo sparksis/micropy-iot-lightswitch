244bdda5286a790b145f3cf5b47aed99f10815eb
Colton English <colton@sparksis.com>
2021-10-07 14:17:29 -0600

    init

--- app/start.py
+++ app/start.py
@@ -1,1 +1,21 @@
-print('Version 2 installed using USB') 
+from machine import Pin
+from time import ticks_ms, sleep_ms
+import app.lightclient as client
+
+pin = Pin(33, Pin.IN, None)
+
+def updateStatus(state):
+    curl('http')
+lastUpdate = 0
+lastValue = 0
+while True:
+    v = pin.value()
+    if ticks_ms() > lastUpdate + 3000:
+        print(v)
+        client.setState('on' if v==1 else 'off')
+        lastUpdate = ticks_ms()
+    elif lastValue != v:
+        lastUpdate = ticks_ms()
+        lastValue = v
+        print(f'debug: updated to {v}@{ticks_ms()}')
+    sleep_ms(20)
