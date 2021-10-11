d450c91e406aabc6a9d9887d8d1877f665ed003e
Colton English <colton@sparksis.com>
2021-10-11 17:09:57 -0600

    Memory improvements (SSL won't work on tiny board)

--- app/lightclient.py
+++ app/lightclient.py
@@ -2,4 +2,4 @@ def setState(state):
     from app.httpclient import HttpClient
 
     client = HttpClient({'Content-Type': 'application/json'})
-    response = client.post('https://maker.ifttt.com/trigger/livingroom_switch_toggled/json/with/key/bpGQDLtVGRPza0T7xPXGaR', data=f'{{"value":"{state}"}}')
+    response = client.post('http://maker.ifttt.com/trigger/livingroom_switch_toggled/json/with/key/bpGQDLtVGRPza0T7xPXGaR', data=f'{{"value":"{state}"}}')
