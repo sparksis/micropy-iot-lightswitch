17853d2bc395cf9b873a7081fbdfd2717008e138
Colton English <colton@sparksis.com>
2021-10-11 17:09:57 -0600

    Memory improvements (SSL won't work on tiny board)

--- app/start.py
+++ app/start.py
@@ -1,17 +1,15 @@
 from machine import Pin
 from time import ticks_ms, sleep_ms
-import app.lightclient as client
 
 pin = Pin(33, Pin.IN, None)
 
-def updateStatus(state):
-    curl('http')
 lastUpdate = 0
 lastValue = 0
 while True:
     v = pin.value()
     if ticks_ms() > lastUpdate + 3000:
         print(v)
+        import app.lightclient as client
         client.setState('on' if v==1 else 'off')
         lastUpdate = ticks_ms()
     elif lastValue != v:
